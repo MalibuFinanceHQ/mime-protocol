48c9c0a8317e2a74ee9264e150e29de0d3432b0d
Aimeric Seguin <aimeric.seguin@gmail.com>
2021-01-15 23:08:27 +0100

    Add Express.js boilerplate

---
+++ packages/server/src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
