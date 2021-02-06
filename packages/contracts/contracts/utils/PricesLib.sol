2caf7342c327e0859b2eed7797845017c7c620e8
Marcello Bardus <marcellobardus8@gmail.com>
2021-02-06 07:53:43 +0100

    contracts: added Uniswap Router interfaces, modified PricesLib to not use a hardcoded WETH address

--- packages/contracts/contracts/utils/PricesLib.sol
+++ packages/contracts/contracts/utils/PricesLib.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "../interfaces/uniswap/IUniswapV2Factory.sol";
 import "../interfaces/uniswap/IUniswapV2Pair.sol";
+import "../interfaces/uniswap/IUniswapV2Router02.sol";
 
 library PricesLib {
     using SafeMath for uint256;
@@ -13,20 +14,22 @@ library PricesLib {
     IUniswapV2Factory public constant UNISWAP_V2_FACTORY =
         IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
 
-    address public constant WETH_TOKEN =
-        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    IUniswapV2Router02 public constant UNISWAP_V2_ROUTER =
+        IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
     function tokenAmountFromWei(address token, uint256 weiToConvert)
         public
         view
         returns (uint256 tokenAmount)
     {
-        if (token == WETH_TOKEN || token == address(0)) {
+        if (token == UNISWAP_V2_ROUTER.WETH() || token == address(0)) {
             return weiToConvert;
         }
         require(weiToConvert > 0, "PricesLib: INSUFFICIENT_AMOUNT");
         IUniswapV2Pair pair =
-            IUniswapV2Pair(UNISWAP_V2_FACTORY.getPair(token, WETH_TOKEN));
+            IUniswapV2Pair(
+                UNISWAP_V2_FACTORY.getPair(token, UNISWAP_V2_ROUTER.WETH())
+            );
         require(address(pair) != address(0), "PricesLib: INVALID_PAIR");
         (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
 
