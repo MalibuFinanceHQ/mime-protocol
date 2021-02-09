206e1ca5bd6c8c29f3ee0881dbceff91fd754b85
Marcello Bardus <marcellobardus8@gmail.com>
2021-02-09 12:10:14 +0100

    uniswap manipulator hotfix

--- packages/contracts/contracts/abi-manipulators/uniswap/UniswapRouterV2Manipulator.sol
+++ packages/contracts/contracts/abi-manipulators/uniswap/UniswapRouterV2Manipulator.sol
@@ -5,12 +5,8 @@ pragma solidity 0.7.5;
 import "../../utils/BytesLib.sol";
 import "../../interfaces/IABIManipulator.sol";
 
-import "hardhat/console.sol";
-
 contract UniswapRouterV2Manipulator is IABIManipulator {
     using BytesLib for bytes;
-
-    uint256 constant methodSignatureLength = 4;
 
     function manipulate(bytes calldata rawTxData, address ctx)
         external
@@ -18,17 +14,6 @@ contract UniswapRouterV2Manipulator is IABIManipulator {
         override
         returns (bytes memory)
     {
-
-        // bytes memory begin = rawTxData.slice(0, 4 + 2 * 32);
-        // bytes memory to = rawTxData.slice(4 + 2 * 32, 32);
-        // bytes memory end = rawTxData.slice(100, rawTxData.length - 100);
-
-        // console.log(begin.length);
-        // console.log(to.length);
-        // console.log(end.length);
-
-        // bytes memory manipulated = begin.concat(abi.encode(ctx)).concat(end);
-
         bytes memory beforeTo = rawTxData.slice(0, 4 + (3 * 32));
         bytes memory to = rawTxData.slice(4 + (3 * 32), 32);
         bytes memory afterTo =
